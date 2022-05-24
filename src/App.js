a7f6e0a449fabb71e51b656bcd2c3331ccdf01fa
MatiDiazz <98648966+MatiDiazz@users.noreply.github.com>
2022-05-23 19:10:27 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
-import CountFunction from './components/Count/CountFunction';
+import ItemCount from './components/ItemCount/ItemCount';
 import NavBar from './components/NavBar/NavBar';
 import ItemListContainer from './components/ItemListContainer/ItemListContainer';
 import CartWidget from './components/CartWidget/CartWidget';
 
 
 function App() {
+  /*const [show, setShow] = useState (true) */
   return (
     <div className="App">
+    {/*<button onClick = {() => setShow(!show)}> En Stock</button> */}
         <NavBar/>
         <CartWidget/>
         <ItemListContainer greeting="Gracias por su compra"/>
-        <CountFunction/>
+        <ItemCount/>
+          {/*{ show ? <ItemCount/> : null}*/}
         <img src={logo} className="App-logo" alt="logo" />
     </div>
   );

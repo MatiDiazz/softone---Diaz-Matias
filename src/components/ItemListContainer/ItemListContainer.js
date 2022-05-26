f4e57ca2e1ca613a7b00eda9004bd8b467f1d011
MatiDiazz <98648966+MatiDiazz@users.noreply.github.com>
2022-05-25 19:40:44 -0700

    Update ItemListContainer.js

--- src/components/ItemListContainer/ItemListContainer.js
+++ src/components/ItemListContainer/ItemListContainer.js
@@ -1,6 +1,23 @@
-const ItemListContainer = (props) =>{
+import {useState, useEffect} from 'react'
+import {getProducts} from '../../asyncmock'
 
-    return <h1> {PaymentResponse.greeting} </h1>
+const ItemListContainer = ({ gretting}) => {
+
+    const[products, setProducts] = useState ([])
+
+    useEffect (() => {
+        getProducts().then(response => {
+            setProducts(response)
+        })
+    })
+
+    return (
+        <div className ='ItemListContainer'>
+            <h1>{ gretting }</h1>
+            { products.map(product => <p>{product.name}</p>)}
+            <ItemList Products = {products}/>
+        </div>
+    )
 
 }
 

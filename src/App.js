687c60ca92e20aa2a3187728315f950c6ecd6d62
MatiDiazz <98648966+MatiDiazz@users.noreply.github.com>
2022-06-06 19:52:40 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import NavBar from './components/NavBar/NavBar';
 import ItemListContainer from './components/ItemListContainer/ItemListContainer';
 import ItemDetailContainer from './components/ItemDetailContainer/ItemDetailContainer';
 import CartWidget from './components/CartWidget/CartWidget';
-
+import { BrowserRouter, Routes, Route} from 'react-router-dom'
 
 const App = () => {
   /*const [show, setShow] = useState (true) */
@@ -14,10 +14,19 @@ const App = () => {
   return (
     <div className="App">
     <button onClick = {() => setPage({...page, path:'list'})}>Lista</button>
-{page.path ==='list'&& <ItemListContainer greeting="Bienvenido" handLePage={setPage}/>}
-{page.path=== 'detail' && <ItemDetailContainer id={page.param}/>}
+{/*{page.path ==='list'&& <ItemListContainer greeting="Bienvenido" handLePage={setPage}/>}*/}
+{/*{page.path=== 'detail' && <ItemDetailContainer id={page.param}/>}*/}
 
-        <NavBar/>
+<BrowserRouter>
+  <NavBar/>
+<Routes>
+  <Route path='/'element={<ItemListContainer/>}/>
+   <Route path='/detail/:productId'element={<ItemDetailContainer/>}/> 
+  <Route path='/about'element={<h1> ABOUT </h1>}/>
+  <Route/>
+</Routes>
+</BrowserRouter>
+        
         <CartWidget/>
         <ItemListContainer greeting="SoftOne"/>
         <ItemCount/>

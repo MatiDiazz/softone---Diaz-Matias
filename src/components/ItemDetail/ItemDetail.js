da25d589e2bebea5360e3b94e56ad2aa388dfcd5
MatiDiazz <98648966+MatiDiazz@users.noreply.github.com>
2022-06-06 20:28:19 -0300

    Update ItemDetail.js

--- src/components/ItemDetail/ItemDetail.js
+++ src/components/ItemDetail/ItemDetail.js
@@ -1,6 +1,6 @@
 import ItemCount from "../ItemCount/ItemCount";
 
-const ItemDetail = ({title}) => {
+const ItemDetail = ({name}) => {
     
     const onAdd = (cant) => {
         console.log('you add $(cant) products to cart')
@@ -9,7 +9,7 @@ const ItemDetail = ({title}) => {
     return(
         <div>
         <h1> Titulo </h1>
-        <h2>{Title}</h2>
+        <h2>{name}</h2>
        
             {products.map(prod => <Item key={prod.id} {...prod}/>)}
         </div>

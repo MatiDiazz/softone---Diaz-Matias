a58185911df93c9a261b7283070c840872dad07c
MatiDiazz <98648966+MatiDiazz@users.noreply.github.com>
2022-06-06 20:38:05 -0300

    Update ItemListContainer.js

--- src/components/ItemListContainer/ItemListContainer.js
+++ src/components/ItemListContainer/ItemListContainer.js
@@ -1,21 +1,32 @@
 import {useState, useEffect} from 'react'
 import {getProducts} from '../../asyncmock'
+import ItemList from '../ItemList/ItemList'
+import { useParams } from 'react-route-dom'
 
-const ItemListContainer = ({ gretting}) => {
+const ItemListContainer = ({ gretting, handLePage }) => {
 
     const[products, setProducts] = useState ([])
+    
+    const {categoryId} =  usParams()
+    console.log(categoryId)
 
     useEffect (() => {
-        getProducts().then(response => {
+        if(!categoryId) {
+            getProducts().then(response => {
             setProducts(response)
         })
-    })
+        } else{
+            getProductsByCategory(categoryId).then(response => {
+                setProducts(response)
+        })
+     }
+    },[])
 
     return (
         <div className ='ItemListContainer'>
             <h1>{ gretting }</h1>
             { products.map(product => <p>{product.name}</p>)}
-            <ItemList Products = {products}/>
+            <ItemList Products = {products} handLePage={handLePage}/>
         </div>
     )
 

2e1534a3562f089c362a91c5c2a6a40f4dfe3595
MatiDiazz <98648966+MatiDiazz@users.noreply.github.com>
2022-06-06 21:18:23 -0300

    Update NavBar.js

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -9,9 +9,9 @@ const NavBar =() =>{
             <h1>SoftOne</h1>
             <div>
                 <link to="/index"><img className="Log" src = "../img/logo.png"></img></link>
-                <link to="/category/notbooks" className='MenuNav'>Notbooks</link>
-                <link to="/category/phones" className='MenuNav'>Celulares</link>
-                <link to="/category/wereables" className='MenuNav'>Wereables </link>
+                <link to="/category/notbooks" className='Option'>Notbooks</link>
+                <link to="/category/phones" className='Option'>Celulares</link>
+                <link to="/category/wereables" className='Option'>Wereables </link>
                 <link to="/"> Sign in </link>
                 <CartWidget  cantidad = {} />
             </div>
